e6f77c0b130e41783d322ecfea602b1f14cc88d4
Trent40 <trent404@gmail.com>
2019-10-15 14:30:54 +0000

    Add v10 with edit/delete campground.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -61,6 +61,41 @@ router.get("/:id", function(req, res){
 	});
 });
 
+// Edit Campground Route
+router.get("/:id/edit", function(req, res){
+	Campground.findById(req.params.id, function(err, foundCampground){
+		if(err){
+			res.redirect("/campgrounds")
+		} else {
+			res.render("campgrounds/edit", {campground: foundCampground});
+		}
+	});
+});
+
+// Update Campground Route
+
+router.put("/:id", function(req, res){
+	// find and update the correct campground
+	Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
+		if (err){
+			 res.redirect("/campgrounds");
+		} else {
+			res.redirect("/campgrounds/" + req.params.id);
+		}
+	});
+});
+
+// Destroy Campground Route
+router.delete("/:id", function(req, res){
+	Campground.findByIdAndRemove(req.params.id, function(err){
+		if(err){
+			res.redirect("/campgrounds");
+		} else {
+			res.redirect("/campgrounds");
+		}
+	});
+});
+
 // Middleware
 
 function isLoggedIn(req, res, next){
