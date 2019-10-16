58866d1dc4d2cc9577eece2fb7d0c611bf9a2320
Trent40 <trent404@gmail.com>
2019-10-16 22:28:45 +0000

    Clear dbs, fix User show page bug.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,6 +2,7 @@ var express    = require("express");
 var	router     = express.Router();
 var	Campground = require("../models/campground");
 
+
 //Index- Show all campgrounds
 
 router.get("/", function(req, res){ 
