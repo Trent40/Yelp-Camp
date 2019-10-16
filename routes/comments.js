92ff8b70d718dc3edfc21a6f711acef7c15e6050
Trent40 <trent404@gmail.com>
2019-10-16 21:05:37 +0000

    Add comment routes, Protect comment routes, Show/hide edit and delete

--- routes/comments.js
+++ routes/comments.js
@@ -56,6 +56,42 @@ router.post("/", isLoggedIn, function(req, res){
   
 });
 
+// Comment Edit Route
+
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res){
+	Comment.findById(req.params.comment_id, function(err, foundComment){
+		if(err){
+			res.redirect("back");
+		} else {
+			res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});
+		}
+	});
+});
+
+//Comment Update
+
+router.put("/:comment_id", checkCommentOwnership, function(req, res){
+	Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err,updatedComment){
+		if(err){
+			res.redirect("back");
+		} else {
+			res.redirect("/campgrounds/" + req.params.id );
+		}
+	});
+});
+
+// Comment Destroy Route
+
+router.delete("/:comment_id", checkCommentOwnership, function(req, res){
+	Comment.findByIdAndRemove(req.params.comment_id, function(err){
+		if(err){
+			res.redirect("back");
+		} else {
+			res.redirect("/campgrounds/" + req.params.id);
+		}
+	});
+});
+
 // Middleware
 
 function isLoggedIn(req, res, next){
@@ -63,6 +99,25 @@ function isLoggedIn(req, res, next){
 		return next();
 	}
 	res.redirect("/login");
-}
+};
+
+function checkCommentOwnership(req, res, next) {
+	if(req.isAuthenticated()){
+		Comment.findById(req.params.comment_id, function(err, foundComment){
+			if(err){
+				res.redirect("back");
+			} else {
+			if(foundComment.author.id.equals(req.user._id)) {
+				next();
+			} else {
+				res.redirect("back");
+			}	
+			}
+		});	
+	} else {
+		res.redirect("back");
+	}
+};
+
 
 module.exports = router;
